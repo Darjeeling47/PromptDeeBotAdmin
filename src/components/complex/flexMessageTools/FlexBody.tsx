5b5178684b331fce36317e3de885d6f7a59d0035
Supawit <supawit.ma@gmail.com>
2024-07-09 13:58:55 +0700

    mockup

--- src/components/complex/flexMessageTools/FlexBody.tsx
+++ src/components/complex/flexMessageTools/FlexBody.tsx
@@ -6,7 +6,7 @@ export default function FlexBody({ content }: { content: any }) {
       className={`w-full break-words px-4 py-2 ${
         "text-[" + content.color + "]"
       } ${"text-" + content.align} ${"text-" + content.type} ${
-        content.bold == "true" ? "font-bold" : "font-normal"
+        content.bold == "true" ? "font-semibold" : "font-normal"
       }`}>
       {content.text}
       {content.seperator == "true" && <hr className='mt-2'></hr>}
